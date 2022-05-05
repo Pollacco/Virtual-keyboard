e7d2ba5becd2cf61c32cd0369fa6843b540ce874
Andrey Sviontek <andrey.sviontek@gmail.com>
2022-05-05 23:01:27 +0500

    init: create base structure of project

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        title: 'Virtual Keyboard',
+        filename: 'index.html',
+        template: 'src/index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
